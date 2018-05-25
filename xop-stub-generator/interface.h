dac200a89066f00404d510d9fbf460f7cd35b245
Thomas Braun <thomas.braun@byte-physics.de>
2018-05-25 12:37:05 +0200

    Add XOP stub generator

---
+++ xop-stub-generator/interface.h
@@ -0,0 +1,6 @@
+/// @file interface.h Available functions in the MIESUtils XOP
+
+/// @brief Return the wave modification count from the XOPToolkit
+///
+/// Builtin into Igor Pro 8.
+variable MU_WaveModCount(WAVE input);
